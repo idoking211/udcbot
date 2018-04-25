4efb55956f6f6f1f6cf36a4b0f7ac435c081fbb4
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-25 15:46:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -160,7 +160,6 @@ bot.on("message", async message => {
     .setColor("#15f153")
     .setThumbnail(bicon)
     .addField("Bot Name", bot.user.username)
-    .addField("Bot Status", ":online~1:")
     .addField("Created On", bot.user.createdAt);
 
     return message.channel.send(botembed);
@@ -195,6 +194,25 @@ bot.on("message", async message => {
     return message.author.send(botembed);
   }
 
+});
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 
 bot.login(process.env.BOT_TOKEN);
