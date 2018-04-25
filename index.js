3432991bbdff21a52b493c93157fe6f6676cf3c2
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-25 18:11:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
+//bot.on("ready", async () => {
+  //console.log(`${bot.user.username} is online!`);
+
+  //bot.user.setActivity("bots", {type: "MAKING"});
+
+  //bot.user.setGame("${server} Servers | /help");
+
 bot.on("ready", async () => {
   console.log(`Bot is Online!`);
 bot.user.setActivity(`${bot.guilds.size} servers | /help`, {type: "WATCHING"});
@@ -24,6 +31,154 @@ bot.user.setActivity(
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+
+  if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("/kick (@user) (reason)\n** **\n**Example:**\n** **\n/kick @user break the rules\n** **\nits will kick the user for brekaing the rules");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("**Kick**")
+    .setColor("#d83c3c")
+    .addField("Kicked", `${kUser}`)
+    .addField("Moderator", `<@${message.author.id}>`)
+    .addField("Kicked In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "mod-log");
+    if(!kickChannel) return message.channel.send("Can't find mod-log channel.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+    return;
+  }
+
+
+  if(cmd === `${prefix}ban`){
+
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!bUser) return message.channel.send("/ban (@user) (time) (reason)\n** **\n**Example:**\n** **\n/ban @user 1 h break the rules\n** **\nits will ban the user for one hour for brekaing the rules");
+    let bReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("**Ban**")
+    .setColor("#bc0000")
+    .addField("**Banned**", `${bUser}`)
+    .addField("**Moderator**", `<@${message.author.id}>`)
+    .addField("Banned In", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", bReason);
+
+    let incidentchannel = message.guild.channels.find(`name`, "mod-log");
+    if(!incidentchannel) return message.channel.send("Can't find mod-log channel.");
+
+    message.guild.member(bUser).ban(bReason);
+    incidentchannel.send(banEmbed);
+
+
+    return;
+  }
+
+
+  if(cmd === `${prefix}report`){
+
+    //!report @ned this is the reason
+
+    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!rUser) return message.channel.send("/report (@user) (reason)\n** **\n**Example:**\n** **\n/report @user break the rules\n** **\nits will report the user for brekaing the rules");
+    let rreason = args.join(" ").slice(22);
+
+    let reportEmbed = new Discord.RichEmbed()
+    .setDescription("Reports")
+    .setColor("#ffdc00")
+    .addField("Reported", `${rUser}`)
+    .addField("Moderator", `${message.author}`)
+    .addField("Channel", message.channel)
+    .addField("Time", message.createdAt)
+    .addField("Reason", rreason);
+
+    let reportschannel = message.guild.channels.find(`name`, "mod-log");
+    if(!reportschannel) return message.channel.send("Couldn't find mod-log channel.");
+
+
+    message.delete().catch(O_o=>{});
+    reportschannel.send(reportEmbed);
+
+    return;
+  }
+
+
+
+
+  if(cmd === `${prefix}serverinfo`){
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+
+
+
+
+  if(cmd === `${prefix}membercount`){
+
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("**Member Count**")
+    .setColor("#eb8f1b")
+    .setThumbnail(sicon)
+    .addField("Members", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+
+
+
+
+  if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
+  }
+
+
+
+
+  if (cmd === `${prefix}say`){
+ 		message.delete()
+         const embed = new Discord.RichEmbed()
+ 		.setColor(0x4d433e)
+ 		.setDescription(args.join(" "));
+ 		message.channel.send({embed})
+}
+
+
+
+
   if(cmd === `${prefix}help`){
 
     let bicon = bot.user.displayAvatarURL;
@@ -44,28 +199,4 @@ bot.user.setActivity(
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === '/ping') {
-    msg.reply(`Pong! The ping is **${(client.ping).toFixed(0)}**ms!  :ping_pong:`)
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '/avatar') {
-    msg.reply("You need to Mention User")
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === '/userinfo') {
-    msg.reply("You need to Mention User")
-  }
-});
-
-client.on('message', message => {
-  if (message.content === 'test') {
-    message.reply("`/help` אני מחוברת, כדאי להתחיל תרשום");
-  }
-});
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
