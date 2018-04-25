de6b5482b7167bc80d3df529322f7da466f9cf6c
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-25 18:21:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
 const bot = new Discord.Client({disableEveryone: true});
 
