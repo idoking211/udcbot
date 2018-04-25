4fc60232db3e7fbcc63e5bfae716d64372bdd1b5
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-25 18:00:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,202 +1,171 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const bot = new Discord.Client({disableEveryone: true});
-
-//bot.on("ready", async () => {
-  //console.log(`${bot.user.username} is online!`);
-
-  //bot.user.setActivity("bots", {type: "MAKING"});
-
-  //bot.user.setGame("${server} Servers | /help");
-
-bot.on("ready", async () => {
-  console.log(`Bot is Online!`);
-bot.user.setActivity(`${bot.guilds.size} servers | /help`, {type: "WATCHING"});
+client.on('ready', () => {
+  client.user.setStatus("online");
+  client.user.setGame("/help | Banana");
 });
 
-// Updates the bot's status if he joins a server
-bot.on("guildCreate", guild => {
-bot.user.setActivity(`${bot.guilds.size} servers | /help`, {type: "WATCHING"});
-});
+const prefix = "/";
+client.on("message", (message) => {
 
-/// Updates the bot's status if he leaves a servers
-bot.on("guildDelete", guild => {
-bot.user.setActivity(
-        `${bot.guilds.size} servers | /help`, {type: "WATCHING"});
-});
+  if (!message.content.startsWith(prefix)) return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  if (message.content === (prefix + "discord")) {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: "\n \n__:קישור לדיסקורד__",
+      description: "** **\nhttps://discord.gg/sdQDrYT",
+      footer: 
+      {
+          icon_url: client.user.avatarURL,
+          text: "Banana"
+      }
+    }});
+  } else
+  if (message.content === (prefix + "أنا ابن العاهرة")) {
+    message.channel.send({embed: {
+      color: 0xffff00,
+      title: "\n \n**                                                                              __:חוקים__**",
+      description: "** **\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n:one: (לא :x: לקלל :scream:  גם בשיחות :telephone_receiver:  וגם בצאטים :speech_left:  (גם אם זה במשחק שמישהו שאיתך\n:joy:  בשיחה הורג אותך בצחוק\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n:two:  לא :x: להספים!\nשאני אומרת ספאם אני מתכוונת\n:rage:  דגשלכדיחלחכחדלכחדלח\nוגם\n:rage:  מילה אחרי מילה אחרי מילה בלי משמעות....\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n:three:  לא :x:  לפרסם שרתי דיסקורד או דברים אחרים בלי אישור \n ( סרטונים :camera:  זה רק ליוטיוברים עם 200+ סאבים)\n:white_check_mark:  אני יגיד בקרוב איך אפשר להשיג אישור\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n:four: לא :x: לשאול דברים אישיים על אחרים\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n:five: לכתוב כל דבר במקום המתאים\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬",
+      footer: 
+      {
+          icon_url: client.user.avatarURL,
+          text: "Banana"
+      }
+    }});
+  } else
+  if (message.content === (prefix + "help")) {
+    message.author.sendMessage({embed: {
+      color: 0xffff00,
+      title: "\n \n__:הפקודות של בננה__",
+      description: "** **\n**/ping** - מראה לך כמה פינג יש לך\n**/discord** - הקישור לדיסקורד\n**/serverinfo** - Server Information\n**/membercount** - Member Count\n**/roles** - מראה לך את הרולים של הסרבר\n**/staff** - מראה לך איזה אנשים נמצאים בצוות שלנו\n**/help** - מראה לך את התפריט הזה",
+      footer: 
+      { 
+          icon_url: client.user.avatarURL,
+          text: "Banana"
+      }
+    }});
+     message.reply(":mailbox_with_no_mail: תבדוק את ההודעות הפרטיות שלך");
+  } else
+    if (message.content == (prefix + "invite")) {
+    message.author.sendMessage("**__Invite The Bot:__**\n \nhttps://discordapp.com/api/oauth2/authorize?client_id=436126804365803520&permissions=0&scope=bot");
+    message.reply("Please check your direct messages :mailbox_with_no_mail:");
+  } else
+  if (message.content === (prefix + "staff")) {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: "\n \n__:צוות השרת שלנו__",
+      description: "** **\n<@&436035474075680768> - <@!378455858100699136> , <@!436161342395908100> , <@311604263379795970> , <@343052053482045450>\n<@&436008360635269120> - <@404663294687838218>\n<@&436008418290171905> - \n<@&436008493900759041> - <@!302904462123466752>\n<@&436008494118731797> - <@331837257004744704>\n<@&436008754794725387> - <@347135990499901442>",
+      footer:
+      {
+          icon_url: client.user.avatarURL,
+          text: "Banana"
+      }
+    }});
+  } else
+  if (message.content === (prefix + "roles")) {
+    message.channel.send({embed: {
+      color: 3447003,
+      title: "\n \n__:הרולים של בננה סרבר__",
+      description: "** **\n**Main Owner**\n**Owner**\n**Co-Owner**\n**ADMIN**\n**MOD**\n**Friend**\n**HELPER**\n**Staff**\n**Bot**\n**Muted**\n**Support Team**",
+      footer: 
+      {
+          icon_url: client.user.avatarURL,
+          text: "Banana"
+      }
+    }});
+ } else
+ if (message.content === (prefix + "membercount")) {
+    message.channel.send({embed: {
+      color: 0xffff00,
+      title: "__Server Members Count:__\n \n",
+      description: "** **",
+      fields: [{
+             name: "Members",
+             value: (message.guild.memberCount)
+           }
+         ],
+         timestamp: new Date(),
+         footer: {
+           icon_url: client.user.avatarURL,
+           text: "Banana"
+       }
+     }});
+ } else
+ if(message.content.startsWith(prefix + "avatar ")) { //IF for the command.
+     if(message.mentions.users.first()) { //Check if the message has a mention in it.
+           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
+           let output = user.tag /*Nickname and Discriminator*/ +
+           "\nAvatar URL: " + user.avatarURL; /*The Avatar URL*/
+           message.channel.sendMessage(output); //We send the output in the current channel.
+    } else {
+          message.reply("Invalid user."); //Reply with a mention saying "Invalid user."
+    }
+ } else
+ if(message.content.startsWith(prefix + "userinfo ")) { //IF for the command.
+     if(message.mentions.users.first()) { //Check if the message has a mention in it.
+           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
+           let output = "UserInfo: \n** **\nUsername: " + (user.username) + "#" + (user.discriminator) + "\n** **" + "\nUserID: " + (user.id) + "\n** **" +  "\nLastMessage: " + "**" + (user.lastMessage)+ "**" + "\n** **" +  "\nI am Bot? : " + (user.bot);
+           message.channel.sendMessage(output); //We send the output in the current channel.
+    } else {
+          message.reply("Invalid user."); //Reply with a mention saying "Invalid user."
+    }
+ } else
+ if (message.content === (prefix + "serverinfo")) {
+    message.channel.send({embed: {
+      color: 0xffff00,
+      title: "__Server Information:__\n \n",
+      description: "** **",
+      fields: [{
+             name: "Server Name",
+             value: (message.guild.name)
+           },
+           {
+            name: "Created On",
+            value: (message.guild.createdAt)
+           },
+           {
+            name: "Joined At",
+            value: (message.member.joinedAt)
+           },
+           {
+            name: "Members",
+            value: (message.guild.memberCount)
+           }
+         ],
+         timestamp: new Date(),
+         footer: {
+           icon_url: client.user.avatarURL,
+           text: "Banana"
+       }
+     }});
+   }
+ });
 
-
-  if(cmd === `${prefix}kick`){
-
-    //!kick @daeshan askin for it
-
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("/kick (@user) (reason)\n** **\n**Example:**\n** **\n/kick @user break the rules\n** **\nits will kick the user for brekaing the rules");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("**Kick**")
-    .setColor("#d83c3c")
-    .addField("Kicked", `${kUser}`)
-    .addField("Moderator", `<@${message.author.id}>`)
-    .addField("Kicked In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "mod-log");
-    if(!kickChannel) return message.channel.send("Can't find mod-log channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-
-    return;
-  }
-
-
-  if(cmd === `${prefix}ban`){
-
-    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("/ban (@user) (time) (reason)\n** **\n**Example:**\n** **\n/ban @user 1 h break the rules\n** **\nits will ban the user for one hour for brekaing the rules");
-    let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
-    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let banEmbed = new Discord.RichEmbed()
-    .setDescription("**Ban**")
-    .setColor("#bc0000")
-    .addField("**Banned**", `${bUser}`)
-    .addField("**Moderator**", `<@${message.author.id}>`)
-    .addField("Banned In", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", bReason);
-
-    let incidentchannel = message.guild.channels.find(`name`, "mod-log");
-    if(!incidentchannel) return message.channel.send("Can't find mod-log channel.");
-
-    message.guild.member(bUser).ban(bReason);
-    incidentchannel.send(banEmbed);
-
-
-    return;
-  }
-
-
-  if(cmd === `${prefix}report`){
-
-    //!report @ned this is the reason
-
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("/report (@user) (reason)\n** **\n**Example:**\n** **\n/report @user break the rules\n** **\nits will report the user for brekaing the rules");
-    let rreason = args.join(" ").slice(22);
-
-    let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Reports")
-    .setColor("#ffdc00")
-    .addField("Reported", `${rUser}`)
-    .addField("Moderator", `${message.author}`)
-    .addField("Channel", message.channel)
-    .addField("Time", message.createdAt)
-    .addField("Reason", rreason);
-
-    let reportschannel = message.guild.channels.find(`name`, "mod-log");
-    if(!reportschannel) return message.channel.send("Couldn't find mod-log channel.");
-
-
-    message.delete().catch(O_o=>{});
-    reportschannel.send(reportEmbed);
-
-    return;
-  }
-
-
-
-
-  if(cmd === `${prefix}serverinfo`){
-
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    return message.channel.send(serverembed);
-  }
-
-
-
-
-  if(cmd === `${prefix}membercount`){
-
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("**Member Count**")
-    .setColor("#eb8f1b")
-    .setThumbnail(sicon)
-    .addField("Members", message.guild.memberCount);
-
-    return message.channel.send(serverembed);
-  }
-
-
-
-
-  if(cmd === `${prefix}botinfo`){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
-
-    return message.channel.send(botembed);
-  }
-
-
-
-
-  if (cmd === `${prefix}say`){
- 		message.delete()
-         const embed = new Discord.RichEmbed()
- 		.setColor(0x4d433e)
- 		.setDescription(args.join(" "));
- 		message.channel.send({embed})
-}
-
-
-
-
-  if(cmd === `${prefix}help`){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("**Commands List**")
-    .setColor("#268ccf")
-    .setThumbnail(bicon)
-    .addField("/kick (user) (reason)", "kick a User.")
-    .addField("/ban (user) (reason)", "ban a User.")
-    .addField("/report (user) (reason)", "report about User.")
-    .addField("/serverinfo", "Server Informations.")
-    .addField("/botinfo", "Bot Informations.")
-    .addField("/membercount", "Member Count.")
-    .addField("/say (message)", "say anything on embed message")
-    .addField("/help", "Help Commands.");
-
-    return message.author.send(botembed);
+client.on('message', msg => {
+  if (msg.content === '/ping') {
+    msg.reply(`Pong! The ping is **${(client.ping).toFixed(0)}**ms!  :ping_pong:`)
   }
 });
 
-bot.login(process.env.BOT_TOKEN);
+client.on('message', msg => {
+  if (msg.content === '/avatar') {
+    msg.reply("You need to Mention User")
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '/userinfo') {
+    msg.reply("You need to Mention User")
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'test') {
+    message.reply("`/help` אני מחוברת, כדאי להתחיל תרשום");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
