f32f1897888ba749e09c86f11769ff343790ba5d
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-25 21:06:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -208,6 +208,17 @@ bot.on("message", (message) => {
 
   if(!message.content.startsWith(prefix)) return;
 
+ if(message.content.startsWith(prefix + "avatar ")) { //IF for the command.
+     if(message.mentions.users.first()) { //Check if the message has a mention in it.
+           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
+           let output = user.tag /*Nickname and Discriminator*/ +
+           "\nAvatar URL: " + user.avatarURL; /*The Avatar URL*/
+           message.channel.sendMessage(output); //We send the output in the current channel.
+    } else {
+          message.reply("You need to Mention someone") //Reply with a mention saying "Invalid user."}
+       }
+     }});
+
   if (!message.guild) return;
 
   if (message.content === '/join') {
@@ -223,17 +234,6 @@ bot.on("message", (message) => {
     }
   }
 });
-
- if(message.content.startsWith(prefix + "avatar ")) { //IF for the command.
-     if(message.mentions.users.first()) { //Check if the message has a mention in it.
-           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
-           let output = user.tag /*Nickname and Discriminator*/ +
-           "\nAvatar URL: " + user.avatarURL; /*The Avatar URL*/
-           message.channel.sendMessage(output); //We send the output in the current channel.
-    } else {
-          message.reply("You need to Mention someone") //Reply with a mention saying "Invalid user."}
-       }
-     }});
 
 bot.on('message', msg => {
   if (msg.content === '/ping') {
