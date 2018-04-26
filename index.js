b64b0a7e6394d96dd70904e66f80a2a790e9a913
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-26 21:29:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
 //welcome join
 bot.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'welcome');
@@ -44,10 +49,6 @@ bot.on('guildMemberRemove', member => {
   channel.send(`${member}, left the Server`);
 });
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
 
 
   if(cmd === `${prefix}kick`){
