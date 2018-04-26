17a9782f7b54a65f303956f022adbec9cd9b2e1a
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-26 20:07:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ bot.on("message", async message => {
 bot.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find('name', 'welcome');
   if (!channel) return;
-  message.channel.SendMessage(`Welcome to the server, ${member}`);
+  message.channel.sendMessage(`Welcome to the server, ${member}`);
 });
 
 //welcome left
 bot.on('guildMemberRemove', member => {
   const channel = member.guild.channels.find('name', 'welcome');
   if (!channel) return;
-  message.channel.SendMessage(`${member}, left the Server`);
+  message.channel.sendMessage(`${member}, left the Server`);
 });
 
 
