aba6446f014824f12f60078a170c64aa92dde6e9
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-27 13:50:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -253,4 +253,27 @@ bot.on('message', msg => {
   }
 });
 
+bot.on('message', msg => {
+  if (message.content == "/unban"){
+    const embed = new Discord.RichEmbed()
+      .setTitle("Unban")
+      .setColor("#86FFF2")
+      .setDescription("Unban a user\n**Parameters:**")
+      .addField("User",
+                "The user to be unbanned", true);
+
+    message.channel.send({embed});
+  }
+  else {
+    let member = message.content.split(/\s+/g)[1];
+    let membername = member.user.username;
+    member.unban();
+    const embed = new Discord.RichEmbed()
+      .setTitle('Unbanned '+membername)
+      .setColor("#CE1126")
+      .setDescription(membername+ " is no longer banned");
+    message.channel.send({embed});
+  }
+};
+
 bot.login(process.env.BOT_TOKEN);
