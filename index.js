a783d199703e54108f115a3fb85d759132bc1192
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-27 14:05:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
+exports.run = (client, message, args) => {
+  if (message.content == "~unban"){
+    const embed = new Discord.RichEmbed()
+      .setTitle("Unban")
+      .setColor("#86FFF2")
+      .setDescription("Unban a user\n**Parameters:**")
+      .addField("User",
+                "The user to be unbanned", true);
+
+    message.channel.send({embed});
+  }
+  else {
+    let member = message.content.split(/\s+/g)[1];
+    let membername = member.user.username;
+    member.unban();
+    const embed = new Discord.RichEmbed()
+      .setTitle('Unbanned '+membername)
+      .setColor("#CE1126")
+      .setDescription(membername+ " is no longer banned");
+    message.channel.send({embed});
+  }
+exports.run = (bot, message, args, Discord, config, commands, customCommands, util, messages) => {
+    let unbanMember = args[0];
+    
+    message.guild.unban(unbanMember);
+
+    message.channel.send(config.memberUnbanned);
+
+    util.log("INFO", "User " + message.author.username + " unbanned player " + unbanMember);
+    
+	message.react('✅');
+}
+};
+
 const bot = new Discord.Client({disableEveryone: true});
 
 //bot.on("ready", async () => {
