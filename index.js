bb2c02f34f90416fcabf12bb0ab90ecae68c1767
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-27 14:27:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require("./config.json");
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
@@ -45,7 +45,7 @@ bot.on("message", async message => {
   if(message.channel.type === "dm") return;
 
 
-  let prefix = config.prefix;
+  let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
