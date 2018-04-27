abe20c148f8ba6319cc47a92d642d7865a4a6465
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-27 14:38:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -217,6 +217,31 @@ bot.on("message", async message => {
 
     return message.author.send(botembed);
   }
+
+module.exports.run = async (client, message) => {
+   const Discord = require('discord.js');
+   var embed = new Discord.RichEmbed()
+  .setAuthor(message.author.tag, message.author.avatarURL)
+  .setDescription("User Information")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Names", "**Username:** " + message.author.username + "\n**Current Nickname:** " + message.member.displayName)
+  .addField("Identity", `**User ID:** ${message.author.id} `)
+  .addField("Create and Join Times", "**Created At:** " + message.member.user.createdAt.toUTCString() + "\n**Joined Guild At:** " + message.member.joinedAt.toUTCString())
+  .setColor('#1fd619')
+   message.channel.send({embed});
+
+  };
+  
+  exports.conf = {
+    aliases: ['uinfo'],
+    guildOnly: false,
+  };
+  exports.help = {
+    name: 'userinfo',
+    description: 'Tells your info.',
+    usage: 'userinfo',
+    category: '- Info Commands',
+  };
 });
 
 const prefix = botconfig.prefix;
