a3681192661adb5284eaa9f370158b9eaa395f2d
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-27 14:19:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ bot.on("message", async message => {
 exports.run = (bot, message, args, Discord, config, commands, customCommands, util, messages) => {
     let unbanMember = args[0];
 
-  if(cmd === `${prefix}report`){
+  if(cmd === `${prefix}unban`){
 
     message.guild.unban(unbanMember);
 
