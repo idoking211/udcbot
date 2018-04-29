237a33e79bb0a0bb8d364cc45f078779a3607bb7
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-29 10:33:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ bot.on("message", async message => {
     .setDescription("**Kick Format**")
     .setColor("#eb8f1b")
     .setThumbnail(sicon)
-    .addField("/kick (@user) (reason)")
+    .addField("/kick @user reason")
     .addField("Example: /kick @user <@!438340566938419210> Break the Rules")
     .addField("Its will kick <@!438340566938419210> for Break the Rules");
 
