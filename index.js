371411c0535d3b4610a6e099f7842730755ec96b
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-29 20:14:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -320,4 +320,14 @@ bot.on('message', msg => {
   }
 });
 
+bot.on('message', message => {
+    if(message.author.bot) return;
+    var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
+    if(re != null){
+        message.delete().then(message => {
+            message.author.send('Sorry, you cannot include links in your messages');
+        });
+    }
+});
+
 bot.login(process.env.BOT_TOKEN);
