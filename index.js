db0b3fb63d76f526c558beb7406301ac51f51e1f
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-29 21:10:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -138,6 +138,28 @@ bot.on("message", async message => {
 
 
 
+    if(command === "${prefix}purge") {
+        let messagecount = parseInt(args[1]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    }
+
+
+
+
   if(cmd === `${prefix}serverinfo`){
 
     let sicon = message.guild.iconURL;
