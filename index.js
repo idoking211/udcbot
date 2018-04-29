c1c19612a05d907dbd6d1b358d890730e01530ad
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-29 21:17:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -375,7 +375,7 @@ bot.on('message', message => {
     var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
     if(re != null){
         message.delete().then(message => {
-            message.author.send('Sorry, you cannot include links in your messages');
+            message.reply('Try to post Link');
         });
     }
 });
