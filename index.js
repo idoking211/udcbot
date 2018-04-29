58c1da0e9bf9d7c570708713856db48eac5d2e59
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-04-29 21:05:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -224,19 +224,19 @@ bot.on("message", async message => {
   let question = args.slice(0).join(" ");
 
   if (args.length === 0)
-  return message.reply('**Invalid Format:** `/poll <Question>`')
+  return message.reply('Invalid Format: /Poll <Question>')
 
   const embed = new Discord.RichEmbed()
   .setTitle("A Poll Has Been Started!")
   .setColor("#5599ff")
-  .setDescription(`${question}`)
-  .setFooter(`Poll Started By: ${message.author.username}`, `${message.author.avatarURL}`)
-
+    .setDescription(`${question}`)
+    .setFooter(`Poll Started By: ${message.author.username}`, `${message.author.avatarURL}`)
   message.channel.send({embed})
-  	//message.delete()  
-
   message.react('👍')
-  .then(() => message.react('👎'));
+  .then(() => message.react('👎'))
+  .then(() => message.react('🤷‍♂️'))
+  .catch(() => console.error('Emoji failed to react.'));
+
 }
 
 
