6c17a99893681d8a4ba92175c45d6a32bc5c221b
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-03 00:44:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -373,14 +373,14 @@ bot.on('message', msg => {
   }
 });
 
-bot.on('message', message => {
-    if(message.author.bot) return;
-    var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
-    if(re != null){
-        message.delete().then(message => {
-            message.reply('Links is not allowed here!');
-        });
-    }
-});
+//bot.on('message', message => {
+    //if(message.author.bot) return;
+    //var re =  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi.exec(message.cleanContent);
+    //if(re != null){
+        //message.delete().then(message => {
+            //message.reply('Links is not allowed here!');
+        //});
+    //}
+//});
 
 bot.login(process.env.BOT_TOKEN);
