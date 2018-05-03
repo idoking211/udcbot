07e20a6c778ecb0ff20479b7221b4af1474f736f
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-03 22:03:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -251,7 +251,8 @@ bot.on("message", async message => {
 
 
 
-
+  if (cmd === `${prefix}poll`){
+ 		message.delete()
 if (!args) return message.reply("You must have something to vote for!")
     if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
         message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
