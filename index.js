baea61c165e294557c576327f0a71d6f525a5fd8
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-04 13:14:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -365,23 +365,18 @@ if( swearWords.some(word => message.content.includes(word)) ) {
     return;
   }
 
-});
-
-const prefix = botconfig.prefix;
-bot.on("message", (message) => {
-
-  if(!message.content.startsWith(prefix)) return;
-
-if(message.content.startsWith(prefix + "avatar ")) { //IF for the command.
+  if (cmd === `${prefix}avatar`){
      if(message.mentions.users.first()) { //Check if the message has a mention in it.
            let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
            let output = user.tag /*Nickname and Discriminator*/ +
            "\nAvatar URL: " + user.avatarURL; /*The Avatar URL*/
            message.channel.sendMessage(output); //We send the output in the current channel.
     } else {
-          message.reply("Invalid user."); //Reply with a mention saying "Invalid user."
+          message.reply("You need Mention a User."); //Reply with a mention saying "Invalid user."
     }
   }});
+
+});
 
 bot.on('message', msg => {
   if (msg.content === '/ping') {
@@ -392,12 +387,6 @@ bot.on('message', msg => {
 bot.on('message', msg => {
   if (msg.content === '/help') {
     msg.reply(`Check your dms`)
-  }
-});
-
-bot.on('message', msg => {
-  if (msg.content === '/avatar') {
-    msg.reply(`You need Mention someone`)
   }
 });
 
