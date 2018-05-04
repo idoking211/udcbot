7e7288ba130080530ee4fa211f8e64e49856f782
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-04 14:51:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -291,8 +291,35 @@ if( swearWords.some(word => message.content.includes(word)) ) {
 }
 
   if (cmd === `${prefix}purge 5`){
-            message.channel.bulkDelete(5)
-            message.channel.send("5 Messages Deleted!")
+     if (!suffix) {
+    var newamount = "2";
+  } else {
+    var amount = Number(suffix);
+    var adding = 1;
+    var newamount = amount + adding;
+  }
+  let messagecount = newamount.toString();
+  msg.channel
+    .fetchMessages({
+      limit: messagecount
+    })
+    .then(messages => {
+      msg.channel.bulkDelete(messages);
+      // Logging the number of messages deleted on both the channel and console.
+      msg.channel
+        .send(
+          "Deletion of messages successful. \n Total messages deleted including command: " +
+            newamount
+        )
+        .then(message => message.delete(5000));
+      console.log(
+        "Deletion of messages successful. \n Total messages deleted including command: " +
+          newamount
+      );
+    })
+    .catch(err => {
+      console.log("Error while doing Bulk Delete");
+      console.log(err)
 }
 
   if (cmd === `${prefix}pinksay`){
