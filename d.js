1cafb878226352d03a9302fefd763650ebd5e266
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-07 16:53:53 +0300

    Update d.js

--- d.js
+++ d.js
@@ -28,7 +28,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("x-help", {type: "WATCHING"});
+  bot.user.setActivity("${bot.guilds.size} servers | /help", {type: "WATCHING"});
 
 });
 
