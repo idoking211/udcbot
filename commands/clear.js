4a18082b3645a16d30a214d93232f4ccb98af5d5
WHumu5AtHZBprr3sfTa0LNBTeA5h2R <38696258+WHumu5AtHZBprr3sfTa0LNBTeA5h2R@users.noreply.github.com>
2018-05-07 16:28:23 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+    message.channel.send(`:white_check_mark: Cleared ${args[0]} messages Complete.`).then(msg => msg.delete(5000));
   });
 }
 
